7141e8a085b7e8e0830f63e9af02c72fe617ea99
Tom Richardson <tomr@apa-tomr.local>
2015-05-14 14:14:12 +0100

    initial commit

---
+++ order.js
@@ -0,0 +1,64 @@
+String.prototype.insert = function (index, string) {
+  if (index > 0)
+    return this.substring(0, index) + string + this.substring(index, this.length);
+  else
+    return string + this;
+};
+
+function orderJobs(instructions) {
+  if(instructions === "") return "";
+  var instruction = instructions.split(',').map(function(element){
+    return new Instruction(element);
+  });
+
+  var instructionMap = [];
+  instruction.forEach(function(element, index) {
+    instructionMap[element.name] = element;
+  });
+
+  var ordered = "";
+
+  instruction.forEach(function(element, index) {
+    ordered = addInstruction(element, instructionMap, ordered);
+  });
+
+  console.log(ordered);
+  return ordered;
+};
+
+function addInstruction(instruction, instructionMap, output, chain){
+  if(typeof chain === 'undefined') chain = "";
+  if(chain.indexOf(instruction.name) !== -1) {
+    throw 'Circular dependency!';
+  }
+  if(output.indexOf(instruction.name) === -1){
+    if(instruction.dependency){
+      output = addInstruction(instructionMap[instruction.dependency], instructionMap, output, chain+instruction.name);
+      var dependencyIndex = output.indexOf(instruction.dependency);
+      if(dependencyIndex >= output.length){
+        output += instruction.name;
+      } else {
+        output = output.insert(dependencyIndex+1, instruction.name);
+      }
+    } else {
+      output += instruction.name;
+    }
+  }
+
+  return output;
+};
+
+function Instruction(instruction){
+  var _name, _dependency;
+
+  _name = instruction[0];
+  _dependency = instruction.length === 6 ? instruction[5] : null;
+
+  if(_name === _dependency) throw "Instruction dependent on self";
+  return {
+    name: _name,
+    dependency: _dependency
+  };
+};
+
+module.exports = orderJobs;
---
+++ spec/spec.js
@@ -0,0 +1,65 @@
+var orderJobs = require('../order.js');
+
+describe("Jobs", function() {
+  it("No jobs", function() {
+    var instructions = "";
+    var result = orderJobs(instructions);
+
+    expect(result).toEqual("");
+  });
+
+  it("Single job", function() {
+    var instructions = "a =>";
+    var result = orderJobs(instructions);
+
+    expect(result).toEqual("a");
+  });
+
+  it("Multiple jobs", function() {
+    var instructions = "a =>,b =>,c=>";
+    var result = orderJobs(instructions);
+
+    expect(result.length).toEqual(3);
+    expect(result.indexOf('a')).not.toEqual(-1);
+    expect(result.indexOf('b')).not.toEqual(-1);
+    expect(result.indexOf('c')).not.toEqual(-1);
+  });
+
+  it("Multiple jobs single dependency", function() {
+    var instructions = "a =>,b => c,c=>";
+    var result = orderJobs(instructions);
+
+    expect(result.length).toEqual(3);
+    expect(result.indexOf('a')).not.toEqual(-1);
+    expect(result.indexOf('b')).not.toEqual(-1);
+    expect(result.indexOf('c')).not.toEqual(-1);
+    expect(result.indexOf('c')).toBeLessThan(result.indexOf('b'));
+  });
+
+  it("Multiple jobs multiple dependencies", function() {
+    var instructions = "a =>,b => c,c => f,d => a,e => b,f =>";
+    var result = orderJobs(instructions);
+
+    expect(result.length).toEqual(6);
+    expect(result.indexOf('a')).not.toEqual(-1);
+    expect(result.indexOf('b')).not.toEqual(-1);
+    expect(result.indexOf('c')).not.toEqual(-1);
+    expect(result.indexOf('d')).not.toEqual(-1);
+    expect(result.indexOf('e')).not.toEqual(-1);
+    expect(result.indexOf('f')).not.toEqual(-1);
+    expect(result.indexOf('c')).toBeLessThan(result.indexOf('b'));
+    expect(result.indexOf('f')).toBeLessThan(result.indexOf('c'));
+    expect(result.indexOf('a')).toBeLessThan(result.indexOf('d'));
+    expect(result.indexOf('b')).toBeLessThan(result.indexOf('e'));
+  });
+
+  it("Self dependent job", function() {
+    var instructions = "a =>,b =>,c => c";
+    expect(function(){orderJobs(instructions);}).toThrow();
+  });
+
+  it("Circular dependency", function() {
+    var instructions = "a =>,b => c,c => f,d => a,e =>,f => b";
+    expect(function(){orderJobs(instructions);}).toThrow();
+  });
+});
